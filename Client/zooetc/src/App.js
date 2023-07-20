6ebdea32e9e0e12f2294cb61cdb10f58f9c00119
Laura Furnivall <laurafurnivall@gmail.com>
2023-07-20 15:41:37 -0500

    Client side set up

---
+++ Client/zooetc/src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Spinner } from 'reactstrap';
+import Header from "./components/Header";
+import ApplicationViews from "./components/ApplicationViews";
+import { onLoginStatusChange } from "./modules/authmanager";
+import { me } from './modules/authmanager';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const [userProfile, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    onLoginStatusChange(setIsLoggedIn);
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      me().then(setUserProfile);
+    } else {
+      setUserProfile(null);
+    }
+  }, [isLoggedIn]);
+
+  if (isLoggedIn === null) {
+    return <Spinner className="app-spinner dark" />;
+  }
+
+  return (
+    <Router>
+      <Header isLoggedIn={isLoggedIn} userProfile={userProfile}/>
+      <ApplicationViews isLoggedIn={isLoggedIn} userProfile={userProfile}/>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Client/zooetc/src/components/ApplicationViews.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+
+export default function ApplicationViews({ isLoggedIn, userProfile }) {
+    return (
+        <main>
+            <Routes>
+                <Route path="/">
+                    <Route
+                        index
+                        element={isLoggedIn ? <Hello /> : <Navigate to="/login" />}
+                    />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="*" element={<p>Whoops, nothing here...</p>} />
+                </Route>
+            </Routes>
+        </main>
+    );
+};
