ae09ca38ae3084283c70743abd9fdadc51d83e57
Laura Furnivall <laurafurnivall@gmail.com>
2023-07-25 11:41:59 -0500

    Working gear list page with search feature

--- Client/zooetc/src/components/Gear/GearList.js
+++ Client/zooetc/src/components/Gear/GearList.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { getAllGearItems } from "../../modules/gearManager";
+import GearCard from "./GearCard";
+import { CardGroup } from "reactstrap";
 
 export default function GearList({ searchTermState }) {
     const [gearItems, setGearItems] = useState([]);
+    const [filteredGear, setFilteredGear] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -11,5 +14,40 @@ export default function GearList({ searchTermState }) {
             setGearItems(items);
         });
     }, []);
-    return <></>
+
+    useEffect(
+        () => {
+            setFilteredGear(gearItems)
+        },
+        [gearItems]
+    )
+
+    useEffect(() => {
+        if (searchTermState === "") {
+            getAllGearItems().then((items) =>{
+                setGearItems(items);
+            })
+        } else {
+            const searchedGear = filteredGear.filter(oneItem => {
+                return oneItem.title.toLowerCase().includes(searchTermState.toLowerCase())
+            })
+            setFilteredGear(searchedGear)
+        }
+    }, [searchTermState])
+
+    return <>
+    <CardGroup className="gearItems">
+        {
+            filteredGear.map((g) => 
+            <GearCard 
+                key={g.id}
+                id={g.id}
+                title={g.title}
+                description={g.description}
+                purchaseUrl={g.purchaseUrl}
+                imageUrl={g.imageUrl}
+            />)
+        }
+    </CardGroup>
+    </>
 }
