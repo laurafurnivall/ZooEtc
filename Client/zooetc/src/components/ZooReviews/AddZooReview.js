ab60c55aecf665180ef77b276469f0e68cdb8916
Laura Furnivall <laurafurnivall@gmail.com>
2023-07-28 21:09:38 -0500

    Finished add and edit zoo review.

--- Client/zooetc/src/components/ZooReviews/AddZooReview.js
+++ Client/zooetc/src/components/ZooReviews/AddZooReview.js
@@ -1,3 +1,177 @@
-export default function AddZooReview () {
-    return <>FEWGDFG</>
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom"
+import { getZoo } from "../../modules/zooManager";
+import { addZooReview } from "../../modules/zooReviewManager";
+import { Rate, Button, Form } from "antd";
+import TextArea from 'antd/es/input/TextArea';
+
+export default function AddZooReview ({userProfile}) {
+    const {id} = useParams();
+    const [zooToReview, setZooToReview] = useState({})
+    const [newZooReview, addNewZooReview] = useState({
+        userId: userProfile.id,
+        zooId: parseInt(id),
+        reviewDate: (new Date).toDateString(),
+        animalCare: 0,
+        benefits: 0,
+        conservationInitiative: 0,
+        culture: 0,
+        inclusivity: 0,
+        leadership: 0,
+        salary: 0,
+        comments: "",
+        isApproved: false
+    })
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getZoo(id).then((fetchedZoo) => {
+            setZooToReview(fetchedZoo);
+        })
+    }, [id]);
+
+    const handleInputChange = (event) => {
+        const value = event.target.value;
+        const key = event.target.name;
+
+        const newReviewCopy = { ...newZooReview }
+
+        newReviewCopy[key] = value;
+
+        addNewZooReview(newReviewCopy);
+    }
+
+    const handleSave = (event) => {
+        event.preventDefault();
+
+        addZooReview(newZooReview)
+        .then(() => navigate(`/Zoos/${id}`))
+    }
+
+    if (userProfile) {
+        return <>
+            <h4>Add a Review to <i>{zooToReview.zooName}</i></h4>
+            <Form
+                name='ZooReviewForm'
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+                style={{
+                    maxWidth: 600,
+                }}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Animal Care"
+                    name="animalCare"
+                    id="animalCare">
+                    <Rate id="animalCare"
+                        onChange={
+                            (value) => {
+                                const copy = {...newZooReview}
+                                copy.animalCare = value
+                                addNewZooReview(copy)
+                            }
+                        }
+                        value={newZooReview.animalCare}
+                        name="animalCare" />
+                </Form.Item>
+                <Form.Item
+                    label="Benefits"
+                    name="benefits"
+                    id="benefits">
+                    <Rate id="benefits"
+                        onChange={ (value) => {
+                            const copy = {...newZooReview}
+                            copy.benefits = value
+                            addNewZooReview(copy)
+                        }}
+                        value={newZooReview.benefits}
+                        name="benefits" />
+                </Form.Item>
+                <Form.Item
+                    label="Conservation Initiative"
+                    name="conservationInitiative"
+                    id="conservationInitiative">
+                    <Rate id="conservationInitiative"
+                        onChange={(value) => {
+                            const copy = {...newZooReview}
+                            copy.conservationInitiative = value
+                            addNewZooReview(copy)
+                        }}
+                        value={newZooReview.conservationInitiative}
+                        name="conservationInitiative" />
+                </Form.Item>
+                <Form.Item
+                    label="Culture"
+                    name="culture"
+                    id="culture">
+                    <Rate id="culture"
+                        onChange={(value) => {
+                            const copy = {...newZooReview}
+                            copy.culture = value
+                            addNewZooReview(copy)
+                        }}
+                        value={newZooReview.culture}
+                        name="culture" />
+                </Form.Item>
+                <Form.Item
+                    label="Inclusivity"
+                    name="inclusivity"
+                    id="inclusivity">
+                    <Rate id="inclusivity"
+                        onChange={(value) => {
+                            const copy = {...newZooReview}
+                            copy.inclusivity = value
+                            addNewZooReview(copy)
+                        }}
+                        value={newZooReview.inclusivity}
+                        name="inclusivity" />
+                </Form.Item>
+                <Form.Item
+                    label="Leadership"
+                    name="leadership"
+                    id="leadership">
+                    <Rate id="leadership"
+                        onChange={(value) => {
+                            const copy = {...newZooReview}
+                            copy.leadership = value
+                            addNewZooReview(copy)
+                        }}
+                        value={newZooReview.leadership}
+                        name="leadership" />
+                </Form.Item>
+                <Form.Item
+                    label="Salary"
+                    name="salary"
+                    id="salary">
+                    <Rate id="salary"
+                        onChange={(value) => {
+                            const copy = {...newZooReview}
+                            copy.salary = value
+                            addNewZooReview(copy)
+                        }}
+                        value={newZooReview.salary}
+                        name="salary" />
+                </Form.Item>
+                <Form.Item label="Comments"
+                    name="comments"
+                    id="comments">
+                    <TextArea
+                        rows={4}
+                        id="comments"
+                        value={newZooReview.comments}
+                        onChange={handleInputChange}
+                        name="comments" />
+                </Form.Item>
+                <Form.Item>
+                    <Button onClick={handleSave}>Save</Button>
+                    <Button onClick={() => navigate("/Gear")}>Cancel</Button>
+                </Form.Item>
+            </Form>
+        </>
+    }
 }
--- Client/zooetc/src/components/ZooReviews/EditZooReview.js
+++ Client/zooetc/src/components/ZooReviews/EditZooReview.js
@@ -1,3 +1,174 @@
+import { Rate, Button, Form } from "antd";
+import TextArea from 'antd/es/input/TextArea';
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom"
+import { editZooReview, getZooReview } from "../../modules/zooReviewManager";
+
 export default function EditZooReview () {
-    return <>DSGSGASG</>
+    const { id } = useParams();
+    const [reviewToEdit, setReviewToEdit] = useState({})
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getZooReview(id).then((fetchedReview) => {
+            setReviewToEdit(fetchedReview);
+        });
+    }, [id]);
+
+    const handleInputChange = (event) => {
+        const value = event.target.value;
+        const key = event.target.id;
+
+        const reviewToEditCopy = { ...reviewToEdit }
+
+        reviewToEditCopy[key] = value;
+
+        setReviewToEdit(reviewToEditCopy);
+    }
+    
+    const handleSave = (event) => {
+        event.preventDefault();
+
+        editZooReview(id, reviewToEdit)
+            .then(() => navigate(`/Zoos/${reviewToEdit.zooId}`))
+    }
+
+    if (reviewToEdit.animalCare) {
+        return <>
+            <h4>Review to Edit</h4>
+            <Form
+                name='ZooReviewForm'
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+                style={{
+                    maxWidth: 600,
+                }}
+                autoComplete="off"
+                initialValues={{
+                    animalCare: reviewToEdit.animalCare,
+                    benefits: reviewToEdit.benefits,
+                    conservationInitiative: reviewToEdit.conservationInitiative,
+                    culture: reviewToEdit.culture,
+                    inclusivity: reviewToEdit.inclusivity,
+                    leadership: reviewToEdit.leadership,
+                    salary: reviewToEdit.salary,
+                    comments: reviewToEdit.comments
+                }}
+            >
+                <Form.Item
+                    label="Animal Care"
+                    name="animalCare"
+                    id="animalCare">
+                    <Rate id="animalCare"
+                        onChange={
+                            (value) => {
+                                const copy = {...reviewToEdit}
+                                copy.animalCare = value
+                                setReviewToEdit(copy)
+                            }
+                        }
+                        value={reviewToEdit.animalCare}
+                        name="animalCare" />
+                </Form.Item>
+                <Form.Item
+                    label="Benefits"
+                    name="benefits"
+                    id="benefits">
+                    <Rate id="benefits"
+                        onChange={
+                            (value) => {
+                                const copy = {...reviewToEdit}
+                                copy.benefits = value
+                                setReviewToEdit(copy)
+                            }
+                        }
+                        value={reviewToEdit.benefits}
+                        name="benefits" />
+                </Form.Item>
+                <Form.Item
+                    label="Conservation Initiative"
+                    name="conservationInitiative"
+                    id="conservationInitiative">
+                    <Rate id="conservationInitiative"
+                        onChange={(value) => {
+                            const copy = {...reviewToEdit}
+                            copy.conservationInitiative = value
+                            setReviewToEdit(copy)
+                        }}
+                        value={reviewToEdit.conservationInitiative}
+                        name="conservationInitiative" />
+                </Form.Item>
+                <Form.Item
+                    label="Culture"
+                    name="culture"
+                    id="culture">
+                    <Rate id="culture"
+                        onChange={(value) => {
+                            const copy = {...reviewToEdit}
+                            copy.culture = value
+                            setReviewToEdit(copy)
+                        }}
+                        value={reviewToEdit.culture}
+                        name="culture" />
+                </Form.Item>
+                <Form.Item
+                    label="Inclusivity"
+                    name="inclusivity"
+                    id="inclusivity">
+                    <Rate id="inclusivity"
+                        onChange={(value) => {
+                            const copy = {...reviewToEdit}
+                            copy.inclusivity = value
+                            setReviewToEdit(copy)
+                        }}
+                        value={reviewToEdit.inclusivity}
+                        name="inclusivity" />
+                </Form.Item>
+                <Form.Item
+                    label="Leadership"
+                    name="leadership"
+                    id="leadership">
+                    <Rate id="leadership"
+                        onChange={(value) => {
+                            const copy = {...reviewToEdit}
+                            copy.leadership = value
+                            setReviewToEdit(copy)
+                        }}
+                        value={reviewToEdit.leadership}
+                        name="leadership" />
+                </Form.Item>
+                <Form.Item
+                    label="Salary"
+                    name="salary"
+                    id="salary">
+                    <Rate id="salary"
+                        onChange={(value) => {
+                            const copy = {...reviewToEdit}
+                            copy.salary = value
+                            setReviewToEdit(copy)
+                        }}
+                        value={reviewToEdit.salary}
+                        name="salary" />
+                </Form.Item>
+                <Form.Item label="Comments"
+                    name="comments"
+                    id="comments">
+                    <TextArea
+                        rows={4}
+                        id="comments"
+                        value={reviewToEdit.comments}
+                        onChange={handleInputChange}
+                        name="comments" />
+                </Form.Item>
+                <Form.Item>
+                    <Button onClick={handleSave}>Save</Button>
+                    <Button onClick={() => navigate(`/Zoos/${reviewToEdit.zooId}`)}>Cancel</Button>
+                </Form.Item>
+            </Form>
+        </>
+    }
 }
